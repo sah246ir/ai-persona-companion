5fe52982d673bac864e77a4b8362733af229399a
sah246ir <sahirchawla2004@gmail.com>
2026-09-02 00:38:19 +0530

    chore: remove Memory boilerplate scaffold

--- src/models/model.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-from sqlmodel import Field, SQLModel
-
-
-class Memory(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    content: str
-    created_at: datetime = Field(default_factory=datetime.utcnow)
